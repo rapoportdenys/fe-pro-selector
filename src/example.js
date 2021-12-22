f25c2743fada37bbe3374c18958f5c39216b48a3
Denys <rapoportden@gmail.com>
2021-12-22 19:27:42 +0200

    add function

--- src/example.js
+++ src/example.js
@@ -6,5 +6,18 @@ const object = {
     },
 };
 
+const selector = (obj, keys) => {
+    for (let i = 0; i < keys.length; i++) {
+        let prop = keys[i];
+        if (obj.hasOwnProperty(prop)) {
+            obj = obj[prop];
+        } else {
+            return ''
+        };
+    };
+    return obj
+};
+
+
 console.log(selector(object, ['test', 'myField', 'value string'])); // => 'result'
 console.log(selector(object, ['test', 'notExistField', 'value string'])); // => ''

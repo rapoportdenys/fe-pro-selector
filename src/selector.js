fc1c673da91b675d015a28b4cf8ff714faa8b5be
Denys <rapoportden@gmail.com>
2021-12-23 18:08:58 +0200

    add selector function in selector.js

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,13 @@
 const selector = (obj, keys) => {
-    // Ваше решение должно быть здесь
+    for (let i = 0; i < keys.length; i++) {
+        let prop = keys[i];
+        if (obj.hasOwnProperty(prop)) {
+            obj = obj[prop];
+        } else {
+            return ''
+        };
+    };
+    return obj
 };
 
 module.exports = selector;
